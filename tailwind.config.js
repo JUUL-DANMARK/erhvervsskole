033bc8bd3e21c30d4cc62b39e5a78feb7c01e6de
HeinF <fh@amujuul.dk>
2024-04-12 15:02:41 +0200

    STYLE: Added ikons and responsive styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'LeagueSpartan': ['"League Spartan"', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
